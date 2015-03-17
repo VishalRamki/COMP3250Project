51094eb25252693973a3e657140b239efeb5b40c
VishalRamki <vishalramkissoon@live.com>
2015-03-17 11:44:33 -0400

    Update tpl.engine.php

--- template/tpl.engine.php
+++ template/tpl.engine.php
@@ -1,7 +1,7 @@
 <?php
 
 	/*
-	 *	Template Engine: 0.0 ALPHA
+	 *	Template Engine: 0.1a ALPHA
 	 *	
 	 *	NB: TEMPLATE ENGINE 0.0 ALPHA IS BASED UPON THESE FOLLOWING RESOURCES:
 	 *		1. http://www.broculos.net/2008/03/how-to-make-simple-html-template-engine.html#.VQcj4uGnboc
@@ -17,95 +17,33 @@
 	 *	The Template Engine can take array and 2d arrays and push the data onto a template file.
 	 *	Additional Work required to prepare template files as well as other features of the 
 	 *	template.
+	 *	
+	 *	10:32 AM [11:35AM] - 17/03/16
+	 *
+	 *	Slight Reordering of code makes it more managable as well as slight performance improvements.
+	 *	In addition, I was able to find a noticeable bug in my code. I was able to quickly correct it. 
+	 *	
 	 *
 	 */
+	 
+	// Global Template Vars 
+	$PATH = "template/";
+	// Template Libraries
+	require_once($PATH. "template.tpl.engine.php");
+	require_once($PATH. "multiview.tpl.engine.php");
 
-class Template {
-	protected $file;
-	protected $values = array();
-	
-	public function __construct($file) {
-		$this->file = $file;
-	}
-	
-	public function set($key, $value) {
-		$this->values[$key] = $value;
-	}
-	
-	public function output() {
-		if (!file_exists($this->file)) {
-			return "Error loading template file ($this->file).";
-		}
-		
-		$output = file_get_contents($this->file);
-		
-		foreach ($this->values as $key => $value) {
-			$tagToReplace = "{".$key."}";
-			$output = str_replace($tagToReplace, $value, $output);
-		}
-		
-		
-		return $output;
-	}
-	
-	// Customer Built Functions:
-	
-	public function buildHashFromArray($array, $params) {
-		/*
-		 *  Needs To Check For 2-D Arrays.
-		 */
-		
-		if (count($array) == count($params)) {
-			foreach($params as $param => $value) {
-				$this->set($value, $array[$param]);
-			}
-		} else {
-			echo $this->errors('array_params_size_error');	
-		}
-	}
-	
-	public function errors($str) {
-		$error = NULL;
-		switch($str) {
-		
-			case "array_params_size_error":
-				$error = "Parameter And Array Sizes Do Not Match.";
-				break;
-			default:
-				$error = "Some Error, who knows.";
-				break;
-		}
-		
-		return $error;
-	}
 
-}
+?>
 
-class MultiView extends Template {
-	
-	public $stackLot = array();
-	
-	public function buildMultiStack($stack, $stackParams) {
-		
-		foreach($stack as $block => $value) {
-			$row = new Template($this->file);
-			$row->buildHashFromArray($value, $stackParams);
-			$this->stackLot[] = $row;
-		}
-	}
-	
-	public function mergeMultiStack() {
-		$output = '';
-		foreach($this->stackLot as $row) {
-			$output .= $row->output();
-		}
-		return $output;
-	}
-	
-}
 
-$profile = new Template("default/userprofile.php");
-$multiProfile = new MultiView("default/userprofiles.php");
+
+<?php
+/*
+
+BASE EXAMPLES;
+
+$profile = new Template($PATH. "default/userprofile.php");
+$multiProfile = new MultiView($PATH. "default/userprofiles.php");
 
 $multiProfile->buildMultiStack(array(array("TE", "Xtra Xtra, Read All About it", "21", "UWI"), array("TR", "QWERTY", '33', "UWI")), array("username", "name", "age", "location"));
 
@@ -114,5 +52,5 @@ $profile->buildHashFromArray(array("Team Cusine's Mega Awesome App (MAA)"), arra
 
 echo $profile->output();
 
-
+*/
 ?>
