5d9ae0ed798f55b33d017628687f16d6bb865fde
VishalRamki <vishalramkissoon@live.com>
2015-03-17 11:41:34 -0400

    Create dev_user_data.php

---
+++ template/default/dev_user_data.php
@@ -0,0 +1,7 @@
+	<div id="user">
+    	<h2>{CustomerName}</h2>
+        <h4>{CustomerID}</h4>
+        	<blockquote>
+        	{CustomerName} is from {Address} which is near {Vicinity}. You may get in contact with them using their email: {Email} or by calling them directly at {PhoneNumber}.
+        	</blockquote>
+    </div>
