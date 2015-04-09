d20692260063f012ecb87f8bfcc6551f6eb8cf5e
VishalRamki <vishalramkissoon@live.com>
2015-04-09 12:18:46 -0400

    Update index.php
    Added Support to perform sorting and limiting of api data.

--- api/index.php
+++ api/index.php
@@ -6,13 +6,7 @@ include('../include/core.db.inc'); // Holds Information Relating to the database
 include('../include/core.statements.inc'); // PHP mySQL Prepared Statments
 include('../include/core.db.funcs.inc'); // Reusable mysql functions.
 
-
 $connection = new mysqli($server, $user, $pass, $dbname);
-
-// TYPED
-
-$const_INT = gettype(0);
-$const_STR = gettype("STRING");
 
 // BASIC API STRUCTURE
 
@@ -72,17 +66,33 @@ if (count($url_elements) == 0) {
 	$bindings = "";
 	$params = array();
 	$identifers = array();
+	$sort_params = array();
+	$sort_ids = array();
+	$sort_bindings = "";
 	$start = 1;
-	$end = count($url_elements) - 1;
+	$end = count($url_elements)-1;
 			
 	for ($x = $start; $x <= $end; $x++) {
 		// First Check For Identifier followed by pushing the data;
-		array_push($identifers, getColumn($url_elements[$x++], $tbl));
-		array_push($params, $url_elements[$x]);
-		if (gettype($url_elements[$x]) == $const_STR)
-			$bindings .= 's';
-		else if (gettype($url_elements[$x]) == $const_INT)
-			$bindings .= 'i';
+		
+		if (strtoupper($url_elements[$x]) == "SORT") {
+			// move forward 
+			array_push($sort_ids, $url_elements[$x++]);
+			array_push($sort_ids, getColumn($url_elements[$x++], $tbl));
+			if ($x <= $end) array_push($sort_ids, $url_elements[$x]);
+		} else if (strtoupper($url_elements[$x]) == "LIMIT") {
+			array_push($sort_ids, $url_elements[$x++]);
+			if ($x <= $end) array_push($sort_ids, $url_elements[$x]);
+		} else {
+			array_push($identifers, getColumn($url_elements[$x++], $tbl));
+			array_push($params, $url_elements[$x]);
+			if (gettype($url_elements[$x]) == $const_STR)
+				$bindings .= 's';
+			else if (gettype($url_elements[$x]) == $const_INT)
+				$bindings .= 'i';
+		}
+		
+
 	}
 				
 	if (count($identifers) > 0 ) {
@@ -94,6 +104,27 @@ if (count($url_elements) == 0) {
 		}
 			
 	}
+	
+	if (count($sort_ids) > 0) {
+		for ($y = 0; $y < count($sort_ids); $y++) {
+			if (strtoupper($sort_ids[$y]) == "SORT") {
+				$y++;
+				$query .= " ORDER BY `". $sort_ids[$y]."` ";
+			} else if (strtoupper($sort_ids[$y]) == "LIMIT") {
+				$query .= " LIMIT ";	
+			}
+			echo $sort_ids[$y]."<br />";
+			if (strtoupper($sort_ids[$y]) == "ASC") {
+				$query .= " ASC ";	
+			} else if (strtoupper($sort_ids[$y]) == "DESC") {
+				$query .= " DESC ";	
+			} else if ($sort_ids[$y] > 0) {
+				$query .= $sort_ids[$y];	
+			}
+		}
+	}
+	
+	echo $query;
 				
 	$response = mysqli_prepared_query($connection, $query, $bindings, $params);
 }
