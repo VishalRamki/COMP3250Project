dfc4662becb085741ad1e7566f623e09f978429f
VishalRamki <vishalramkissoon@live.com>
2015-04-14 09:48:28 -0400

    Create js.php

---
+++ admin/js.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+  This is a admin-panel wide code. Needed for all pages.
+*/
+?>
+<script type="text/javascript">
+$(function() {
+	var height = $("#body").height();
+	
+	$("#menu").height(height);
+	
+});
+</script>
